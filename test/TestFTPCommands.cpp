61674390d657e1bacb0a48ec699c66324428ffca
Pierre Falez <pierre.falez@gmail.com>
2015-02-16 17:06:49 +0100

    Parse pasv

--- test/TestFTPCommands.cpp
+++ test/TestFTPCommands.cpp
@@ -52,6 +52,10 @@ public:
 
 	}
 
+	/*
+	 *	Commande
+	 */
+
 	Packet initializeConnection(unsigned int port) {
 		_socket.connect(IP::Address("127.0.0.1"), port);
 
@@ -85,8 +89,85 @@ public:
 		return packet_answer;
 	}
 
+	Packet commandePasv() {
+
+		Packet packet;
+		packet << "PASV";
+		_socket.send(packet);
+
+		Packet packet_answer;
+		_socket.receive(packet_answer);
+
+		return packet_answer;
+	}
+
+	/*
+	 *	Parse Packet
+	 */
+
+	void parsePasvInfo(std::string str) {
+		unsigned int start_index = str.find_first_of("(");
+		unsigned int end_index = str.find_last_of(")");
+
+		std::string raw_address = str.substr(start_index, end_index-start_index);
+
+		char* c_raw_addr = new char[raw_address.size()+1];
+
+		strcpy(c_raw_addr, raw_address.c_str());
+
+		char* token = strtok(c_raw_addr, ",");
+		unsigned int i;
+
+		std::string address;
+		unsigned int port = 0;
+		for(i=0; token != nullptr; i++) {
+
+			int factor = 1;
+			char* end_nb;
+
+			switch(i) {
+			case 0:
+				address = token;
+				break;
+			case 1:
+			case 2:
+			case 3:
+				address += "."+std::string(token);
+				break;
+			case 4:
+				factor = 256;
+			case 5:
+				{
+					int nb = strtol(token, &end_nb, 10);
+
+					if(token == end_nb) {
+						delete c_raw_addr;
+						THROW(UnrecognizedMessageException, "PASV "+raw_address, std::string(token)+" is an incorrecte number");
+					}
+					port += factor*nb;
+				}
+				break;
+			default:
+				delete c_raw_addr;
+				THROW(UnrecognizedMessageException, "PASV "+raw_address, "too many arguments");
+			}
+
+			token = strtok(NULL, ",");
+		}
+		delete c_raw_addr;
+		if(i != 6)
+			THROW(UnrecognizedMessageException, "PORT "+raw_address, "no enough argument");
+
+		std::cout << "Address=" << address << ", port=" << port << std::endl;
+		_dataAddress = IP::Address(address);
+		_dataPort = port;
+	}
+
 private:
 	TCP::Socket _socket;
+
+	IP::Address _dataAddress;
+	unsigned int _dataPort;
 };
 
 TEST_F(TestFTPCommands, testConnection) {
@@ -113,8 +194,6 @@ TEST_F(TestFTPCommands, testUser) {
 TEST_F(TestFTPCommands, testPass) {
 	FTPClient client;
 
-	try {
-
 	client.initializeConnection(currentPort);
 	client.commandeUser("falezp");
 	Packet packet_pass = client.commandePass("mdp1");
@@ -122,10 +201,21 @@ TEST_F(TestFTPCommands, testPass) {
 	std::string packet_answer_pass_code;
 	packet_pass >> packet_answer_pass_code;
 	ASSERT_EQ(std::to_string(AnswerLoginOk::Code), packet_answer_pass_code);
+}
 
-	} catch(Exception& e) {
-		std::cout << e.getMessage() << std::endl;
-	}
+TEST_F(TestFTPCommands, testPasv) {
+	FTPClient client;
+
+	client.initializeConnection(currentPort);
+	client.commandeUser("falezp");
+	client.commandePass("mdp1");
+	Packet packet_pasv = client.commandePasv();
+
+	std::string packet_answer_pasv_code;
+	packet_pasv >> packet_answer_pasv_code;
+	ASSERT_EQ(std::to_string(AnswerEnteringPassiveMode::Code), packet_answer_pasv_code);
+
+	client.parsePasvInfo(std::string(packet_pasv.getBuffer(), packet_pasv.getSize()));
 }
 /*
 TEST_F(TestFTPCommands, testLIST) {
